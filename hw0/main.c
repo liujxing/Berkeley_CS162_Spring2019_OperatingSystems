d7f1c23e2e4fcc18169e5ae2f3906620e7826145
Jiaxing Liu <liujxing@gmail.com>
2019-04-01 13:31:09 +0000

    finished lab0

--- hw0/main.c
+++ hw0/main.c
@@ -3,8 +3,28 @@
 
 int main() {
     struct rlimit lim;
-    printf("stack size: %ld\n", 0L);
-    printf("process limit: %ld\n", 0L);
-    printf("max file descriptors: %ld\n", 0L);
+    
+    // get maximum stack size
+    if (getrlimit(RLIMIT_STACK, &lim) != 0) {
+        printf("The stack limit cannot be obtained\n");
+    } else {
+        printf("maximum stack size: %ld\n", (long)lim.rlim_cur);
+    }
+    
+    
+    // get maximum number of processes
+    if (getrlimit(RLIMIT_NPROC, &lim) != 0) {
+        printf("The number of processes cannot be obtained\n");
+    } else {
+        printf("maximum number of processes: %ld\n", (long)lim.rlim_cur);
+    }
+    
+    // get maximum number of file descriptor
+    if (getrlimit(RLIMIT_NOFILE, &lim) != 0) {
+        printf("The number of file descriptor cannot be obtained\n");
+    } else {
+        printf("max file descriptors: %ld\n", (long)lim.rlim_cur);
+    }
+    
     return 0;
 }
--- hw0/map.c
+++ hw0/map.c
@@ -8,14 +8,14 @@ int foo;
 int recur(int i) {
     /* A stack allocated variable within a recursive function */
     int j = i;
-
+    
     /* TODO 1: Fix this so it prints the address of j */
-    printf("recur call %d: stack@ %p\n", i, j);
-
+    printf("recur call %d: stack@ %p\n", i, (void*)&j);
+    
     if (i > 0) {
         return recur(i - 1);
     }
-
+    
     return 0;
 }
 
@@ -25,21 +25,21 @@ int stuff = 7;
 int main(int argc, char *argv[]) {
     /* A stack allocated variable */
     int i;
-
+    
     /* Dynamically allocate some stuff */
     char *buf1 = malloc(100);
     /* ... and some more stuff */
     char *buf2 = malloc(100);
-
+    
     printf("_main  @ %p\n", main);
     printf("recur @ %p\n", recur);
-
+    
     /* TODO 2: Fix to get address of the stack variable */
-    printf("_main stack: %p\n", i);
-
+    printf("_main stack: %p\n", (void*)&i);
+    
     /* TODO 3: Fix to get address of a static variable */
-    printf("static data: %p\n", stuff);
-
+    printf("static data: %p\n", (void*)&stuff);
+    
     printf("Heap: malloc 1: %p\n", buf1);
     printf("Heap: malloc 2: %p\n", buf2);
     recur(3);
